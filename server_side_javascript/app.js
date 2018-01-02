341ab96cc7bf5b2ce6e37498a3f378893c45498a
Jbok <0614bok@gmail.com>
2018-01-03 02:08:52 +0900

    Express Template Engine
    1.Useage template engine
    -npm install jade --save //installation jade
    -app.set('views', './views'); //template directory
    -app.set('view engine', 'jade'); //template engine to use jade
    -app.get('/template', function(req,res){ //routing, if client access to
     /template then excute function
    -res.render('temp'); //use render in template engine
    
    2.Jade's function
    -res.render{'temp', {time:Date(), _title:'Jade'}}; //add parameter to
     use

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.locals.pretty = true; //make temp.jade's code pretty
+app.set('views', './views');
+app.set('view engine', 'jade');
 app.use(express.static('public')) //static file location
 const port = 3000;
+app.get('/template', function(req,res){
+  res.render('temp', {time:Date(), _title:'Jade'});
+});
 app.get('/', function(req, res){
   res.send('Hello home page');
-})
+});
 app.get('/dynamic', function(req,res){
   var time = Date();
   var lis ='';
@@ -28,13 +34,13 @@ app.get('/dynamic', function(req,res){
     </body>
   </html>`;
   res.send(output);
-})
+});
 app.get('/route',function(req, res){
   res.send('Hello Tools, <img src="tools.png">')
-})
+});
 app.get('/login', function(req, res){
   res.send('<h1>Login please<h1>');
-})
+});
 app.listen(port, function(){
   console.log('Connected %d port!',port);
 });
