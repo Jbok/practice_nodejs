1032f1c8cb782b70b512ae38efe5c1882a940fb0
Jbok <0614bok@gmail.com>
2018-01-03 22:57:32 +0900

    Make simple practice web app
    -this web app use file for data rather data base. it is bad way.
    
    1.npm supervisor
    -npm install supervisor -g //supervisor can detect change without
     restart node js
    -fs.readdir(paht, callback) //make file in path to array
    -res.render('view', {topics: files}) //use file's contents using topics
     in view.js
    -app.get(['/topic', 'topic/:id']) //using [], can route several path.

---
+++ server_side_javascript/app_file.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.set('views', './views_file');
+app.set('view engine', 'jade');
+// app.get('/topic/new',function(req,res){
+//   res.render('new');
+// })
+app.get(['/topic', '/topic/:id'], function(req, res){
+  var id = req.params.id;
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error data');
+    }
+      if(id){//There is id
+        if(id==='new'){
+          res.render('new', {topics: files})
+        }
+        else{
+          fs.readFile('data/'+id, 'utf8', function(err, data){
+            if(err){
+              console.log(err);
+              res.status(500).send('Internal Server Error data+id');
+            }
+            res.render('view', {topics: files, title: req.params.id, text: data})
+          })
+        }
+      }
+      else{
+        //There is no id mainpage
+        res.render('view', {topics: files, title: 'Welcome', text: 'Hello, JavaScript for server'})
+      }
+  })
+})
+app.post('/topic', function(req,res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/'+title);
+  });
+})
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
+})
